66eccda76958407efc0565087b7eaae72e9f3d75
Gordon <gordon.zhang@coxautoinc.com>
2022-11-05 19:34:17 -0400

    first commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/goxlarge/gopage
+
+go 1.19
+
+require golang.org/x/net v0.1.0
---
+++ page/page.go
@@ -0,0 +1,63 @@
+package page
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+
+	"golang.org/x/net/html"
+)
+
+func GetAttribute(n *html.Node, key string) (string, bool) {
+	for _, attr := range n.Attr {
+		if attr.Key == key {
+			return attr.Val, true
+		}
+	}
+
+	return "", false
+}
+
+func RenderNode(n *html.Node) string {
+	var buf bytes.Buffer
+	w := io.Writer(&buf)
+	err := html.Render(w, n)
+	if err != nil {
+		return ""
+	}
+	return buf.String()
+}
+
+func CheckClass(n *html.Node, className string) bool {
+	if n.Type == html.ElementNode {
+		s, ok := GetAttribute(n, "class")
+		if ok && s == className {
+			return true
+		}
+	}
+
+	return false
+}
+
+func GetElementsByClass(n *html.Node, className string) *[]*html.Node {
+	var targets []*html.Node
+	getElementsByClass(n, className, &targets)
+	return &targets
+}
+
+func getElementsByClass(n *html.Node, className string, targets *[]*html.Node) {
+	if CheckClass(n, className) {
+		*targets = append(*targets, n)
+		return
+	}
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		getElementsByClass(c, className, targets)
+	}
+}
+
+func Traverse(n *html.Node) {
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		fmt.Println(RenderNode(c))
+		Traverse(c)
+	}
+}
